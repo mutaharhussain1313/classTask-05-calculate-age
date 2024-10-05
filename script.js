b162282b9ba6ecd70d222d8cf4f2827b30d7bf3a
unknown <mutaharhussain1313@gmail.com>
2024-10-05 13:24:12 +0500

    add function and if statement for invalid date

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
-
-
-  
-  // Attach event listener to the "Calculate" button
-  document.getElementById("cal").addEventListener("click", calculateAge);
+ document.getElementById("cal").addEventListener("click", calculateAge);
 
   function calculateAge() {
     const inputDate = new Date(document.getElementById("date-input").value);
-    const today = new Date();
-  
-    // Handle invalid input
+    const currentDate = new Date();
+
     if (isNaN(inputDate)) {
-      alert("Please enter a valid date.");
+      alert("Plz Eneter your correct date");
       return;
     }
-  
-    // Calculate age components
-    let years = today.getFullYear() - inputDate.getFullYear();
-    let months = today.getMonth() - inputDate.getMonth();
-    let days = today.getDate() - inputDate.getDate();   
-  
-  
-    // Adjust if necessary
-    if (days < 0) {
-      months--;
-      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-    }
-    if (months < 0) {
-      years--;
-      months += 12;
-    }
-  
-    // Update the display
-    document.getElementById('years').innerHTML = `${years} <span>Years</span>`;
-    document.getElementById('months').innerHTML = `${months} <span>Months</span>`;
-    document.getElementById('days').innerHTML = `${days} <span>Days</span>`;
-  
   }
